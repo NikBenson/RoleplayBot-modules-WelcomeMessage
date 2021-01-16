e7c5fd10f7081cae62ef240c3869314699eda12f
NikBenson <NikBenson@pm.me>
2021-01-16 14:55:34 +0100

    implemented module jar loader and manager

---
+++ src/com/github/NikBenson/RoleplayBot/modules/welcomeMessage/WelcomeMessage.java
@@ -0,0 +1,16 @@
+package com.github.NikBenson.RoleplayBot.modules.welcomeMessage;
+
+import com.github.NikBenson.RoleplayBot.modules.RoleplayBotModule;
+import net.dv8tion.jda.api.entities.Guild;
+
+public class WelcomeMessage extends RoleplayBotModule {
+	@Override
+	public void load(Guild guild) {
+
+	}
+
+	@Override
+	public void unload(Guild guild) {
+
+	}
+}
